da56aaf887f2fe2b55edcfdb2156eaa9cb88bd78
smallmacro <smallmacro@gmail.com>
2022-12-02 10:10:20 -0500

    Add second e2e spec test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,15 @@
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach(() => {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('should navigate from the main page to a page of a particular Pokeman', () => {
+    cy.contains('ivysaur').click()
+    cy.url().should('include','/pokemon/ivysaur')
   })
 })
